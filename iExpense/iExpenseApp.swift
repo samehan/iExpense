02b06afe16c68a91cc23da3931fb054a3dfdbb9c
samehan <samehansonwane@gmail.com>
2024-08-04 14:20:05 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Samehan Sonwane on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
